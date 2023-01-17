8656ac5c049664f2e7380a6780d5310c908e0c52
baba-s <shotababa1206@gmail.com>
2023-01-17 19:46:12 +0900

    add

---
+++ Editor/MeshRendererInspector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    [CustomEditor( typeof( MeshRenderer ) )]
+    internal sealed class MeshRendererInspector : Editor
+    {
+        private Editor             m_editor;
+        private SerializedProperty m_sortingOrderProperty;
+        private SerializedProperty m_sortingLayerIDProperty;
+
+        public override void OnInspectorGUI()
+        {
+            if ( m_editor == null )
+            {
+                CreateCachedEditor
+                (
+                    targetObjects: targets,
+                    editorType: Type.GetType( "UnityEditor.MeshRendererEditor, UnityEditor" ),
+                    previousEditor: ref m_editor
+                );
+            }
+
+            m_sortingOrderProperty   ??= serializedObject.FindProperty( "m_SortingOrder" );
+            m_sortingLayerIDProperty ??= serializedObject.FindProperty( "m_SortingLayerID" );
+
+            m_editor.OnInspectorGUI();
+            serializedObject.Update();
+
+            SortingLayerEditorUtilityInternal.RenderSortingLayerFields
+            (
+                sortingOrder: m_sortingOrderProperty,
+                sortingLayer: m_sortingLayerIDProperty
+            );
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Kogane.MeshRendererInspector
+# Kogane Mesh Renderer Inspector
+
+![ScreenShot00129](https://user-images.githubusercontent.com/6134875/212878813-c591e085-4fa3-497b-9449-2e0217c149ff.png)
+
+MeshRenderer の Inspector
